e31a6fbf490d981d5ec7ed4bc0a0a3fb72f53a44
UdayKumarChitturi <chitturiuday@gmail.com>
2026-05-07 16:01:32 -0400

    backend version 2 update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const http = require('http');
+const http = require("http");
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("Backend CI/CD Running 🚀");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Backend Deployed via CI/CD 🚀 - Version 2");
 });
 
 server.listen(PORT, () => {
